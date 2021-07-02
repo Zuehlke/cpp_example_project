ec13376cd8fd2e90641284210974ebce4f430a4b
Arne <Arne.Mertz@zuehlke.com>
2021-07-02 15:34:04 +0200

    update Dockerfile: correctly install conan

--- docker/build-dev-image.sh
+++ docker/build-dev-image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+CONTAINER=cpp_docker_env
+
+if /usr/bin/docker ps | grep "${CONTAINER}" > /dev/null; then
+  docker-compose down
+fi
+
 DOCKER_BUILDKIT=1 docker -l debug build \
     --target cpp-docker-dev \
     --build-arg DEV_USER=dev \
