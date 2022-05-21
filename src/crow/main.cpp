ac9c4c318f22ccad72a1148eadb0172f44fd474c
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2022-05-21 21:52:50 +0200

    formatted crow

--- src/crow/main.cpp
+++ src/crow/main.cpp
@@ -3,47 +3,38 @@
 
 class a : public crow::returnable
 {
-public:
-  a() : returnable("text/plain"){};
+  public:
+    a() : returnable("text/plain"){};
 
-  std::string dump() const override
-  {
-    return "ABCDF";
-  }
+    std::string dump() const override { return "ABCDF"; }
 };
 
 int main()
 {
-  constexpr uint16_t port = 8080;
-  std::vector<int> values;
-  crow::SimpleApp app;
+    constexpr uint16_t port = 8080;
+    std::vector<int> values;
+    crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/")
-  ([]() {
-    return "Hello world";
-  });
+    CROW_ROUTE(app, "/")
+    ([]() { return "Hello world"; });
 
-  CROW_ROUTE(app, "/json")
-  ([] {
-    return crow::json::wvalue({ { "message", "Hello, World!" }, {"message2", "Hello, World.. Again!"} });
-  });
+    CROW_ROUTE(app, "/json")
+    ([]() { return crow::json::wvalue({ { "message", "Hello, World!" }, { "message2", "Hello, World.. Again!" } }); });
 
-  CROW_ROUTE(app, "/custom")
-  ([]() {
-    return a();
-  });
+    CROW_ROUTE(app, "/custom")
+    ([]() { return a(); });
 
-  CROW_ROUTE(app, "/hello/<int>").methods(crow::HTTPMethod::POST)([&values](int count) {
-    values.push_back(count);
-    return crow::response(std::to_string(count));
-  });
+    CROW_ROUTE(app, "/hello/<int>").methods(crow::HTTPMethod::POST)([&values](int count) {
+        values.push_back(count);
+        return crow::response(std::to_string(count));
+    });
 
-  CROW_ROUTE(app, "/hello/").methods(crow::HTTPMethod::GET)([&values]() {
-    crow::json::wvalue returnValue;
-    returnValue["values"] = values;
-    return returnValue;
-  });
+    CROW_ROUTE(app, "/hello/").methods(crow::HTTPMethod::GET)([&values]() {
+        crow::json::wvalue returnValue;
+        returnValue["values"] = values;
+        return returnValue;
+    });
 
-  app.port(port).run();
-  return 0;
+    app.port(port).run();
+    return 0;
 }
